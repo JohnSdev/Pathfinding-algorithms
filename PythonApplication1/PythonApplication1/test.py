3058c4b2677d82eedf24faf1139f4415cefb0036
swanjohn79 <swanjohn79@outlook.com>
2017-12-09 08:52:31 +0100

    tedt

--- PythonApplication1/PythonApplication1/test.py
+++ PythonApplication1/PythonApplication1/test.py
@@ -48,7 +48,7 @@ def minCost(cost, m, n):
  
 
     tc = [[0 for x in range(C)] for x in range(R)]
-    step=1
+    step=2
     tc[0][0] = cost[0][step]#col 3
     
     # Initialize first column of total cost(tc) array
@@ -62,7 +62,7 @@ def minCost(cost, m, n):
     # Construct rest of the tc array
     for i in range(1, m+1):
         for j in range(1, n+1):
-            tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1]) + cost[i][j+step]
+            tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i-1][j+1]) + cost[i][j+step]
     for x in tc:
         print(x)
    #print (*tc)
