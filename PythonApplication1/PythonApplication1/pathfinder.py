ee23b7ff39a55d5ea14d196031c79006919dc22e
swanjohn79 <swanjohn79@outlook.com>
2017-12-12 16:12:37 +0100

    Funkis DP

--- PythonApplication1/PythonApplication1/pathfinder.py
+++ PythonApplication1/PythonApplication1/pathfinder.py
@@ -6,6 +6,7 @@ class Pathfinder:
         self._map = map
 
     def findCheapestPath(self):
+        matrix=self._map.getMatrix()
         cost=0
         # Mark's silly algorithm for finding the cheapest path:
         #   Use random logic - virtually gauranteed to give the correct answer
@@ -14,27 +15,95 @@ class Pathfinder:
         #         the correct answer when we get it which is a bummer.
 
         # Starting at a random position on the left:
-        for x in range(5,7):
+        for x in range(0,1):
+            m=479
+            n=843
             starting_row = x
 
             # Search for one random path:
-            ( cost, path ) = self.minCost( starting_row )
 
+            #self.minCost( starting_row )
+            self.dynamicP(matrix, m, n)
+
+            #print(path)
         # It is the only path we have found, visualise it:
        # self._visualiser.addPath(path)
 
         # The only path so it must also be the best path, visualise that:
-            self._visualiser.setBestPath(path)
+            #self._visualiser.setBestPath(path)
 
         # And the cost of this so called "best" path:
-            self._visualiser.setBestPathCost( cost )
-
+            #self._visualiser.setBestPathCost( cost )
+            
         # What next?  Can you do better than random?
         # TODO:  Step 1 - a greeedy algorithm from a random starting position
         # TODO:  Step 2 - best greedy of all possible starting positions
         # TODO:  Step 3 - improve even more!
         return
 
+    def dynamicP(self, grid, m, n):
+        cost=grid
+        R = 900
+        C = 900
+
+        total_cost=[]
+        tc = [[0 for x in range(C)] for x in range(R)]
+
+        step=0
+        #tc[0][0] = cost[0][step]#col 3
+    
+    
+        #for i in range(1, m+1):
+        #    tc[i][0] = abs(cost[i-1][0] - cost[i][step])
+        #    #tc[i][0] = tc[i-1][0] + cost[i][step]
+ 
+  
+        #for j in range(1, n+1):
+        #    tc[0][j] = abs(cost[0][j-1] - cost[0][j+step])
+ 
+        print("DP Matrix")
+        #for i in range(0, m):
+        #    for j in range(0, n):
+        #        next_min=min(cost[i-1][j-1]+tc[i-1][j-1], cost[i][j-1]+tc[i][j-1], cost[i+1][j-1]+tc[i+1][j-1])
+        #        tc[i][j] = abs(next_min - cost[i][j+step])
+        #        #add append of abs to list
+        #        total_cost.append(tc[i][j])
+
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if j <=1:
+                    BU=100000000000
+                else:
+                    BU=abs(cost[j][i] - cost[j-1][i-1]) + tc[j-1][i-1]
+                if j >=477:
+                    BD=1000000000
+                else:
+                
+                    BD=abs(cost[j][i] - cost[j+1][i-1]) + tc[j+1][i-1]
+                B=abs(cost[j][i] - cost[j][i-1]) + tc[j][i-1]
+                next_min=min(BU, B, BD)
+                
+                print("NM {} on col:{}, pos {}".format(next_min, i, cost[j][i]))
+                tc[j][i] = next_min
+                
+                #add append of abs to list
+                total_cost.append(tc[j][i])
+
+        #for x in tc:
+        #    print(x)
+
+        print("")
+        print("Original Matrix {}, {}".format(m,n))
+        costrow=[]
+        for x in range(0,478):
+            costrow.append(tc[x][843])
+        print(costrow)
+        print("Total Cost :",tc[m][n])
+        #print (*tc)
+
+        #Construct path
+        
+        return tc[m][n]
 
     def minCost(self, start):
         cost=0
@@ -60,13 +129,13 @@ class Pathfinder:
         rotatedmap = list(map(list, new))
         R = 900
         C = 600
-        m=843
-        n=start
+        m=840
+        n=5
         #col=0
         
         #path=[col]
         
-        step=5
+        step=0
 
         tc = [[0 for x in range(C)] for x in range(R)]
         
@@ -83,14 +152,20 @@ class Pathfinder:
         # Construct rest of the tc array
         for i in range(1, m+1):
             for j in range(1, n+1):
-                tc[i][j] = min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1]) + rotatedmap[i][j+step]         
+                next_min=min(tc[i-1][j-1], tc[i-1][j], tc[i][j-1])
+                tc[i][j] = abs(next_min - rotatedmap[i][j+step])     
                        
+
+        #Construct path
+       
+
+
         costn.append(int(tc[m][n]))        
         #path.append(col)
         #print (costn)
         total=(sorted(costn))
-        best=min(costn)
-
+       
+        print(tc[m][n])
         r=0
         path=[ row ]
         row = row + r
@@ -99,10 +174,10 @@ class Pathfinder:
         if row > rows-1:
         
             row = rows-1
-        print (row)
+        
         path.append(row)
 
-        return (best, path)
+        return 
       
 
     #        total.append(sum(costn))
--- PythonApplication1/PythonApplication1/test-abs.py
+++ PythonApplication1/PythonApplication1/test-abs.py
@@ -12,6 +12,7 @@ for i in range(0,2):
         a2[x] += min(a1[x], a1[x+1])
         print(a2)
     map.pop(-1)
+
     map[-1] = a2
 print(map[0][0])
 
@@ -41,47 +42,112 @@ from itertools import accumulate
 """
  
 def minCost(cost, m, n):
-    R = 10
-    C = 10
+    R = 4
+    C = 4
 
- 
     total_cost=[]
     tc = [[0 for x in range(C)] for x in range(R)]
+
     step=0
-    tc[0][0] = cost[0][step]#col 3
+    #tc[0][0] = cost[0][step]#col 3
     
     
-    for i in range(1, m+1):
-        tc[i][0] = abs(cost[i-1][0] - cost[i][step])
-        #tc[i][0] = tc[i-1][0] + cost[i][step]
+    #for i in range(1, m+1):
+    #    tc[i][0] = abs(cost[i-1][0] - cost[i][step])
+    #    #tc[i][0] = tc[i-1][0] + cost[i][step]
  
   
-    for j in range(1, n+1):
-        tc[0][j] = abs(cost[0][j-1] - cost[0][j+step])
+    #for j in range(1, n+1):
+    #    tc[0][j] = abs(cost[0][j-1] - cost[0][j+step])
  
-    
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            next_min=min(cost[i-1][j-1], cost[i-1][j], cost[i][j-1])
-            tc[i][j] = abs(next_min - cost[i][j+step]) + tc[i-1][j-1]
+    print("DP Matrix")
+    #for i in range(0, m):
+    #    for j in range(0, n):
+    #        next_min=min(cost[i-1][j-1]+tc[i-1][j-1], cost[i][j-1]+tc[i][j-1], cost[i+1][j-1]+tc[i+1][j-1])
+    #        tc[i][j] = abs(next_min - cost[i][j+step])
+    #        #add append of abs to list
+    #        total_cost.append(tc[i][j])
+
+    for i in range(1, n+1):
+        for j in range(0, m):
+            print(cost[j][i])
+            print(cost[j-1][i-1])
+            print(cost[j][i-1])
+            print(cost[j+1][i-1])
+            print(abs(cost[j][i+step] - cost[j-1][i-1])+tc[j-1][i-1])
+            print(abs(cost[j][i+step] - cost[j][i-1]) +tc[j][i-1])
+            print(abs(cost[j][i+step] - cost[j+1][i-1])+tc[j+1][i-1])
+
+            next_min=min(abs(cost[j][i+step] - cost[j-1][i-1]) + tc[j-1][i-1], 
+                         abs(cost[j][i+step] - cost[j][i-1]) + tc[j][i-1],
+                         abs(cost[j][i+step] - cost[j+1][i-1]) + tc[j+1][i-1])
+            print("NM",next_min)
+            tc[j][i] = next_min
             #add append of abs to list
-            total_cost.append(tc[i][j])
+            total_cost.append(tc[j][i])
+
     for x in tc:
         print(x)
-   #print (*tc)
-    print(sum(total_cost))
-    return tc[m][n]
+
+    print("")
+    print("Original Matrix {}, {}".format(m,n))
+    for x in cost:
+        print(x)
+    print("Total Cost :",tc[m][n])
+    #print (*tc)
+
+    #Construct path
+        
+    return tc
 
 # Driver program to test above functions
-cost = [[1, 1, 1, 1, 1, 1, 1, 1],
-        [2, 32, 2, 2, 2, 2, 2, 2],
-        [2, 31, 1, 1, 1, 1, 1, 1],
-        [2, 31, 1, 1, 1, 1, 1, 1],
-        [2, 31, 1, 1, 1, 1, 1, 1],
-        [2, 31, 1, 1, 1, 1, 1, 1]]
+
 #for x in range(0,4):
 #    print("rad:{}, costnad: {}".format(x, minCost(cost, 3, x)))
-print(minCost(cost, 5, 1), "Target is 4")
+
+
+def backtrack( a, b, LCS ):
+    s = ""
+    # read the substring out from the matrix
+    i = a
+    j = b
+    while i>0 and j > 0:
+        if LCS[i][j] == LCS[i-1][j]:
+            i -= 1
+        elif LCS[i][j] == LCS[i][j-1]:
+            j -= 1
+        else:
+            # must have been a match
+            s = a[i-1] + s
+            i -= 1
+            j -= 1
+    return s
+
+trow=2
+tcol=2
+
+cost2 = [[1, 1, 2, 1],
+        [2, 3, 2, 2],
+        [2, 3, 2, 1],
+        [2, 3, 2, 1],
+        [2, 3, 2, 1]]
+
+cost = [[2000, 2500, 1500, 1500],
+        [1800, 1900, 4500, 4500],
+        [1500, 3000, 2000, 2000],
+        [0, 0, 0, 0],
+        [2, 3, 2, 1],
+        [2, 3, 2, 1]]
+
+tc=minCost(cost, trow, tcol)
+path = backtrack( trow,tcol, tc )
+
+
+
+
+
+
+
 
 
 # This code is contributed by Bhavya Jain
