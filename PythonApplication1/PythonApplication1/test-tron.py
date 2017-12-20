cbefc79bdc28c9c0b6f98e5ad030222e254dc01a
swanjohn79 <swanjohn79@outlook.com>
2017-12-20 23:06:35 +0100

    fots tron path

--- PythonApplication1/PythonApplication1/test-tron.py
+++ PythonApplication1/PythonApplication1/test-tron.py
@@ -27,7 +27,7 @@ def minCost(cost, m, n):
 #    print("rad:{}, costnad: {}".format(x, minCost(cost, 3, x)))
 
 #Backtrack example LCS
-def backtrack(tc, rows, cols):
+def backtrack(tc, rows, cols, grid):
     pathlist=[]
     debuglist=[]
     debubstorage=[]
@@ -41,46 +41,122 @@ def backtrack(tc, rows, cols):
 
     #if direction == "LEFT":
     while cols >0:
-            
+        if grid[rows][cols] == 2:
+            break 
         if rows >=4:
             nextMin=max(tc[rows-1][cols], tc[rows][cols-1])
         elif rows <=0:
             nextMin=max(tc[rows][cols-1], tc[rows+1][cols])
+        elif cols <= 0:
+            nextMin=max(tc[rows+1][cols], tc[rows][cols+1])
         else:
-            nextMin=max(tc[rows-1][cols], tc[rows][cols-1], tc[rows+1][cols])
+            nextMin=max(tc[rows-1][cols], tc[rows][cols-1], tc[rows+1][cols],tc[rows][cols+1])
                 
         if  nextMin == tc[rows+1][cols]:         
             newdir = "DOWN"
             pathlist.append("DOWN")
-            tc[rows+1][cols] = 0
+            
             #debuglist.append([rows+1, cols-1])
-            rows +=1          
+            rows +=1
+            tc[rows-1][cols] = 0
         elif nextMin == tc[rows][cols-1]:
             newdir = "LEFT"
             pathlist.append("LEFT")
             #debuglist.append([rows, cols-1])
             cols -=1
+            tc[rows][cols+1] = 0
+        elif nextMin == tc[rows][cols+1]:
+            newdir = "RIGHT"
+            pathlist.append("RIGHT")
+            #debuglist.append([rows, cols-1])
+            cols +=1
             tc[rows][cols-1] = 0
         elif nextMin == tc[rows-1][cols]:
             #print(nextMin, rows, cols)
             newdir = "UP"
             pathlist.append("UP")
             rows -= 1
-            tc[rows][cols-1] = 2
+            tc[rows+1][cols] = 0
+         
+        print(nextMin, rows,cols)
+            
+    return newdir, pathlist
+
+def backtrack2(tc, rows, cols, grid):
+    pathlist=[]
+    debuglist=[]
+    debubstorage=[]
+    pathstorage=[]
+    rows=rows
+    newdir=""
+
+    #Start traverse bottom-up
+    #for i in range(4,5):
+    #    rows = i
+
+    #if direction == "LEFT":
+    while cols <5:
+        if grid[rows][cols] == 2:
+            break 
+        if rows >=5:
+            nextMin=max(tc[rows-1][cols], tc[rows][cols-1])
+        elif rows <=0:
+            nextMin=max(tc[rows][cols-1], tc[rows+1][cols])
+        elif cols <= 0:
+            nextMin=max(tc[rows+1][cols], tc[rows][cols+1])
+        else:
+            nextMin=max(tc[rows-1][cols], tc[rows][cols-1], tc[rows+1][cols],tc[rows][cols+1])
+        print(nextMin, rows, cols)        
+        if  nextMin == tc[rows+1][cols]:         
+            newdir = "DOWN"
+            pathlist.append("DOWN")
+            
+            #debuglist.append([rows+1, cols-1])
+            rows +=1
+            tc[rows-1][cols] = 0
+        elif nextMin == tc[rows][cols-1]:
+            newdir = "LEFT"
+            pathlist.append("LEFT")
+            #debuglist.append([rows, cols-1])
+            cols -=1
+            tc[rows][cols+1] = 0
+        elif nextMin == tc[rows][cols+1]:
+            newdir = "RIGHT"
+            pathlist.append("RIGHT")
+            #debuglist.append([rows, cols-1])
+            cols +=1
+            tc[rows][cols-1] = 0
+        elif nextMin == tc[rows-1][cols]:
+            #print(nextMin, rows, cols)
+            newdir = "UP"
+            pathlist.append("UP")
+            rows -= 1
+            tc[rows+1][cols] = 0
          
         print(nextMin, rows,cols)
             
     return newdir, pathlist
 
 trow=4
-tcol=4
+tcol=5
 
-cost2 = [[0, 0, 0, 0, 0, 0],
-         [0, 0, 0, 0, 0, 0],
-         [0, 0, 0, "O", 0, 0],
-         [0, 0, 2, "O", 0, 0],
-         [0, 0, 2, "O", 0, 1]]
+cost2 = [[0, 0,  0,   0,  0,  0],
+         [0, 0,  0,   0,  0,  0],
+         [0, 0, "O", "O", 0,  0],
+         [0, 0,  2,  "O", 0,  0],
+         [0, 0,  2,  "O", 0,  1]]
 
+cost3 = [[0, 0,  0,   0,   0,  0],
+         [0, 0,  0,   0,   0,  0],
+         [0, 0,  0,  "O", "O", 0],
+         [0, 0,  2,  "O",  0,  0],
+         [0, 0,  2,  "O",  0,  1]]
+
+rev =   [[0, 0,  0,   0,   0,  0, 0],
+         [0, 0,  0,   0,   0,  0, 0],
+         [0, 0,  0,  0, "O", 0, 0],
+         [0, 1,  0,  0,  "O",  2, 0],
+         [0, 1,  0,  0,  "O",  0, 0]]
 cost = [[2000, 2500, 1500, 1500],
         [1800, 1900, 4500, 4500],
         [1500, 3000, 2000, 2000],
@@ -89,13 +165,14 @@ cost = [[2000, 2500, 1500, 1500],
         [2, 3, 2, 1]]
 
 #To test the algorithm
-tc=minCost(cost2, trow, tcol)
+tc=minCost(rev, trow, tcol)
 for x in tc:
     print(x)
 
-path, pathlist = backtrack(tc, trow,tcol)
-print(pathlist)
-
+#path, pathlist = backtrack(tc, trow,tcol, cost3)
+#print(pathlist)
+path2, pathlist2 = backtrack2(tc, 4,1, rev)
+print(pathlist2)
 
 
 
